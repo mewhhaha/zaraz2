53d1688a307e5925ff22f9d68785489afd19dac2
mewhhaha <kopatheonlyone@hotmail.com>
2025-11-16 15:54:35 +0100

    fix: add viewport fit covber

--- web/app/document.tsx
+++ web/app/document.tsx
@@ -45,7 +45,7 @@ export default function Document({
         <link rel="icon" type="image/svg" href={iconUrl}></link>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content"
+          content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content, viewport-fit=cover"
         ></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
